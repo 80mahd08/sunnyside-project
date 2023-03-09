b88623b7a2474c5d769e5e88a095a0c333b0974f
80mahd08 <amarimahdi8@gmail.com>
2023-03-09 11:46:38 +0100

    prit

--- public/ts/main.ts
+++ public/ts/main.ts
@@ -1,8 +1,7 @@
-let menuLogo = <HTMLSpanElement> document.querySelector('.lines')
-let nav = document.querySelector('nav');
+let menuLogo = <HTMLSpanElement>document.querySelector(".lines");
+let nav = document.querySelector("nav");
 
-
-menuLogo.addEventListener('click' , ()=>{
-    menuLogo.classList.toggle('menuLogo')
-    nav!.classList.toggle("dispNone")
-})
+menuLogo.addEventListener("click", () => {
+  menuLogo.classList.toggle("menuLogo");
+  nav!.classList.toggle("dispNone");
+});
